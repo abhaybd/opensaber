62820c96006b1092aa650b5d5f74b5b6cb09bd55
Abhay D <a.deshpande012@gmail.com>
2021-03-31 22:33:12 -0700

    Added rebiasing code for audio, remixed ignition for better flow

--- assets/sound/convert_wav.py
+++ assets/sound/convert_wav.py
@@ -13,10 +13,16 @@ if __name__ == "__main__":
     name, wav, dest = sys.argv[1:]
 
     audio = wavio.read(wav)
-    sound_str = ", ".join([str(x[0]) for x in audio.data])
+    data = [x[0] for x in audio.data]
+    bits = 8 * audio.sampwidth
+    # 8 bit pcm is unsigned, but 16 and 24 are signed, so make unsigned by adding bias
+    if bits != 8:
+        bias = 1 << (bits - 1)
+        data = [x + bias for x in data]
+    sound_str = ", ".join([str(x) for x in data])
     with open(dest, "w") as f:
         f.write("#pragma once\n\n")
         f.write("constexpr struct {\n"
                 f"    const uint freq = {audio.rate}; // Hz\n"
-                f"    const uint8_t sound[{len(audio.data)}] = {{{sound_str}}};\n"
+                f"    const uint{bits}_t sound[{len(audio.data)}] = {{{sound_str}}};\n"
                 f"}} {name};\n")
